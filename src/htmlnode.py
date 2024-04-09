ac1cac6c8f619448987fd1868b036dc7ffa04ed4
huang-brian15 <huangbrian15@gmail.com>
2024-04-09 14:20:26 -0400

    Added leafnode and leafnode tests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -19,3 +19,18 @@ class HTMLNode:
     
     def __repr__(self) -> str:
         return f"HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props})"
+
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag: str = None, value: str = None, props: dict = None):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self) -> str:
+        if self.value == None:
+            raise ValueError("Invalid HTML: no value")
+        if self.tag == None:
+            return self.value
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+    
+    def __repr__(self) -> str:
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,20 +1,37 @@
 import unittest
 
 from htmlnode import HTMLNode
+from htmlnode import LeafNode
 
 
 class TestHTMLNode(unittest.TestCase):
-    def test_props1(self):
+    def test_html_attr1(self):
         d = {"href": "https://www.google.com", "target": "_blank"}
-        html_node = HTMLNode(None, None, None, d)
+        html_node = HTMLNode(tag=None, value=None, children=None, props=d)
         truth = " href=\"https://www.google.com\" target=\"_blank\""
         self.assertEqual(html_node.props_to_html(), truth)
 
-    def test_props2(self):
+    def test_html_attr2(self):
         d = {"href": "https://www.google.com", "target": "_blank", "test_key": "test_value"}
-        html_node = HTMLNode(None, None, None, d)
+        html_node = HTMLNode(tag=None, value=None, children=None, props=d)
         truth = " href=\"https://www.google.com\" target=\"_blank\" test_key=\"test_value\""
         self.assertEqual(html_node.props_to_html(), truth)
+
+    def test_leaf_node_no_tag(self):
+        leaf_node = LeafNode(tag=None, value="test_value")
+        truth = f"test_value"
+        self.assertEqual(leaf_node.to_html(), truth)
+    
+    def test_leaf_node_no_value(self):
+        leaf_node = LeafNode(tag="p", value=None)
+        with self.assertRaises(ValueError):
+            leaf_node.to_html()
+
+    def test_leaf_node_full(self):
+        d = {"href": "https://www.google.com", "target": "_blank"}
+        leaf_node = LeafNode(tag="a", value="my value", props=d)
+        truth = f"<a href=\"https://www.google.com\" target=\"_blank\">my value</a>"
+        self.assertEqual(leaf_node.to_html(), truth)
 
 
 def main():
